afd3df09db79793d6c4877117bb286e62ab9050d
Phillip Schichtel <phillip.public@schich.tel>
2014-09-28 20:42:19 +0200

    More restructuring

---
+++ syslog/src/main/java/de/cubeisland/engine/logscribe/target/SyslogTarget.java
@@ -0,0 +1,18 @@
+package de.cubeisland.engine.logscribe.target;
+
+import de.cubeisland.engine.logscribe.LogEntry;
+import de.cubeisland.engine.logscribe.LogTarget;
+
+public class SyslogTarget extends LogTarget
+{
+    // TODO implement me
+    @Override
+    protected void publish(LogEntry entry)
+    {
+    }
+
+    @Override
+    protected void onShutdown()
+    {
+    }
+}
